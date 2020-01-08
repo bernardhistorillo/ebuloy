29f312cbb39c3c13df7a7f290ce65bc67a59ecbd
Bernard <benard.historillo@bicol-u.edu.ph>
2020-01-08 21:59:09 +0800

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends("layouts.layout")
+
+@section("title")
+<title>ebuloy</title>
+@stop
+
+@section("content")
+    <div class="">
+        <div class="container">
+            <div class="card">
+                <div class="card-header">Hello World</div>
+                <div class="card-body">
+                    Lorem ipsum dolor sit amet.
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
